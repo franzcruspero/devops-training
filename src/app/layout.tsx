7b57d12d52106b73b1d097b646f7e7b0409a4408
Franz Andrei Cruspero <franz.cruspero@gmail.com>
2023-10-02 11:34:38 +0800

    Hello world

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <h3> Hello World! </h3>
       <body className={inter.className}>{children}</body>
     </html>
   )
