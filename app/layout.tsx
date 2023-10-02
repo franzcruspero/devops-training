c41fef17fa19d05fad342c39fa0374be798205b5
Franz Andrei Cruspero <franz.cruspero@gmail.com>
2023-10-02 11:48:24 +0800

    nice ka ngart

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <h3> Hello World! </h3>
       <body className={inter.className}>{children}</body>
     </html>
   )
